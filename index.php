5aa4804ce0a319b8d309c48273f563f8edffa1bc
mackliu <macklun@gmail.com>
2020-05-08 10:43:56 +0800

    include 的使用

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include_once "base.php";?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php include "title.php" ;?></title>
+</head>
+<body>
+<a href="invoice.php">發票</a><br>
+<a href="award.php">對獎</a>    
+</body>
+</html>
